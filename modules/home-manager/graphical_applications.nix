378905826600f953cda09f72bdb88cf3086312c6
Joshua Hernandez <joshua1571@users.noreply.github.com>
2025-12-16 13:53:27 -0800

    Fixed lock file

--- modules/home-manager/graphical_applications.nix
+++ modules/home-manager/graphical_applications.nix
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-# Not sure I like this very specific flag here
-=======
->>>>>>> c3909fc67a700c48014a0c1731e581ec1fec402c
 {
   pkgs,
   lib,
@@ -13,29 +9,19 @@
   home.packages =
     with pkgs;
     [
-<<<<<<< HEAD
       brave
-=======
       # GUI Applications
->>>>>>> c3909fc67a700c48014a0c1731e581ec1fec402c
       spotify
       discord
       obsidian
       ticktick
       ktailctl # Requires tailscale service
-<<<<<<< HEAD
-=======
       brave
->>>>>>> c3909fc67a700c48014a0c1731e581ec1fec402c
       bitwarden-desktop
       github-desktop
       libreoffice-qt6
       deskflow
-<<<<<<< HEAD
       #opensnitch # Requires opensnitch service
-=======
-      opensnitch # Requires opensnitch service
->>>>>>> c3909fc67a700c48014a0c1731e581ec1fec402c
       zapzap
     ]
     ++ lib.optionals emulation [
@@ -47,11 +33,6 @@
 
   programs = {
     mpv.enable = true;
-<<<<<<< HEAD
     zathura.enable = false;
-=======
-    zathura.enable = true;
->>>>>>> c3909fc67a700c48014a0c1731e581ec1fec402c
   };
-
 }
