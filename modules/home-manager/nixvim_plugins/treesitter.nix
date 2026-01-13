18448db0e0853b886ab6b18d3cf596bd4161b882
Joshua Hernandez <joshua1571@users.noreply.github.com>
2026-01-12 16:31:06 -0800

    Removed treesitter nixvim plugin

--- modules/home-manager/nixvim_plugins/treesitter.nix
+++ modules/home-manager/nixvim_plugins/treesitter.nix
@@ -9,21 +9,10 @@
         highlight.enable = true;
         indent.enable = true;
       };
-      folding = false;
+      folding.enable = false;
     };
 
-    treesitter-refactor = {
-      enable = true;
-
-      settings = {
-        highlight_definitions = {
-          enable = true;
-
-          # Set to false if you have an `updatetime` of ~100.
-          clear_on_cursor_move = false;
-        };
-      };
-    };
+    treesitter-context.enable = true;
 
     hmts.enable = true;
   };
